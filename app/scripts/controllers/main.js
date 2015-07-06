b35f3b93406bb32d0ca87c63bf2fd05aaacbb5b7
Rajesh Verma <rajesh.verma@optimusinfo.com>
2015-07-06 18:48:34 +0530

    comit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name testprojectApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the testprojectApp
+ */
+angular.module('testprojectApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
